204a60708bf0114152c1b3551610ad2928765230
Ismail <x.iml.7122@gmail.com>
2024-06-18 10:07:43 +0700

    perubahan kecil

--- src/components/EduHistory.tsx
+++ src/components/EduHistory.tsx
@@ -6,19 +6,19 @@ import Link from "next/link";
 const EduHistory = () => {
   const educationData = [
     {
-      institution: "SD 3 Adiwarno",
+      institution: "SDN 3 Adiwarno",
       degree: "Sekolah Dasar",
       period: "2013 - 2019",
       link: "https://www.google.com/maps/place/SD+Negeri+3+Hadiwarno/@-6.8236739,110.9019524,17z/data=!3m1!4b1!4m6!3m5!1s0x2e70cf86ffffffff:0xefee82e387358d01!8m2!3d-6.8236792!4d110.9045273!16s%2Fg%2F11r7pnyc8?entry=ttu",
     },
     {
-      institution: "SMP 2 Mejobo",
+      institution: "SMPN 2 Mejobo",
       degree: "Sekolah Menengah Pertama",
       period: "2019 - 2022",
       link: "https://www.google.com/maps/place/SMP+N+2+Mejobo/@-6.8297114,110.8944126,17z/data=!3m1!4b1!4m6!3m5!1s0x2e70c5792ac04ba3:0xdf7f666fec4f39fd!8m2!3d-6.8297167!4d110.8969875!16s%2Fg%2F1hm4ch61h?entry=ttu",
     },
     {
-      institution: "SMK 2 Kudus",
+      institution: "SMKN 2 Kudus",
       degree: "Sekolah Menengah Kejuruan",
       period: "2022 - 2025",
       link: "https://www.google.com/maps/place/SMK+Negeri+2+Kudus/@-6.7442212,110.8953406,17z/data=!3m1!4b1!4m6!3m5!1s0x2e70d080403423c7:0x58974ec098194b5d!8m2!3d-6.7442265!4d110.8979155!16s%2Fg%2F1ptxv84n4?entry=ttu",
